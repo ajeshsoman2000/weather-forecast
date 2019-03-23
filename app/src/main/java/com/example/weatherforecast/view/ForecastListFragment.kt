16d191a5e1acc65d37cb1b135d650755010c3e95
ajeshsoman2000 <ajeshsoman2000@gmail.com>
2019-03-23 13:27:34 +0530

    Made the forecast list screen as a fragment. Activity now has only parent framelayout

---
+++ app/src/main/java/com/example/weatherforecast/view/ForecastListFragment.kt
@@ -0,0 +1,120 @@
+package com.example.weatherforecast.view
+
+import android.content.Context
+import android.os.Bundle
+import android.view.KeyEvent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.DefaultItemAnimator
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.weatherforecast.R
+import com.example.weatherforecast.model.WeatherResponse
+import com.example.weatherforecast.utils.isNetworkConnectionAvailable
+import com.example.weatherforecast.view.adapters.WeatherForecastRecyclerViewAdapter
+import com.example.weatherforecast.viewmodel.MainViewModel
+import com.example.weatherforecast.viewmodel.ViewModelFactory
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_forecast_list.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+class ForecastListFragment: Fragment() {
+
+    private var weatherDetail: LiveData<WeatherResponse>? = null
+    lateinit var mainViewModel: MainViewModel
+    private val backgroundScope = CoroutineScope(Dispatchers.IO)
+    private val mainScope = CoroutineScope(Dispatchers.Main)
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
+            inflater.inflate(R.layout.fragment_forecast_list, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        mainViewModel = ViewModelProviders.of(activity as MainActivity, ViewModelFactory()).get(MainViewModel::class.java)
+
+        rv_weather_forecast.layoutManager = LinearLayoutManager(activity)
+        rv_weather_forecast.itemAnimator = DefaultItemAnimator()
+
+        weatherDetail = mainViewModel.getWeatherForecast()
+
+        if ((weatherDetail as LiveData<WeatherResponse>).value != null) {
+            if (mainViewModel.getWeatherForecast().value is WeatherResponse.Success) {
+                (activity as MainActivity).window.setSoftInputMode(InputMethodManager.HIDE_NOT_ALWAYS)
+                tv_placeholder.visibility = View.GONE
+                (mainViewModel.getWeatherForecast().value as WeatherResponse.Success).result.forecast.forecastday.let {
+                    rv_weather_forecast.adapter =
+                        WeatherForecastRecyclerViewAdapter(
+                            activity as MainActivity,
+                            it
+                        )
+                }
+            }
+        }
+
+
+        et_city.setOnEditorActionListener { _, actionId, event ->
+            if ((event != null && (event.keyCode == KeyEvent.KEYCODE_ENTER)) ||
+                (actionId == EditorInfo.IME_ACTION_DONE)) {
+                fetchForecast()
+            }
+            false
+        }
+
+        tv_get_forecast.setOnClickListener { fetchForecast() }
+    }
+
+    private fun setObserver(){
+        (weatherDetail as LiveData<WeatherResponse>).observe(activity as MainActivity,
+            Observer<WeatherResponse> { t ->
+                if (t is WeatherResponse.Success) {
+                    tv_placeholder.visibility = View.GONE
+                    rv_weather_forecast.adapter =
+                        WeatherForecastRecyclerViewAdapter(
+                            activity as MainActivity,
+                            t.result.forecast.forecastday
+                        )
+                } else {
+                    tv_placeholder.visibility = View.VISIBLE
+                    Toast.makeText(activity as MainActivity, (t as WeatherResponse.Error).message, Toast.LENGTH_SHORT).show()
+                    rv_weather_forecast.adapter =
+                        WeatherForecastRecyclerViewAdapter(
+                            activity as MainActivity,
+                            null
+                        )
+                }
+            })
+    }
+    private fun fetchForecast() {
+        if ((activity as MainActivity).isNetworkConnectionAvailable()) {
+            if (et_city.text.toString().trim().isNotEmpty()) {
+                val imm = (activity as MainActivity).getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                imm.toggleSoftInput(0, InputMethodManager.HIDE_NOT_ALWAYS)
+                pb_fetch_forecast.visibility = View.VISIBLE
+                backgroundScope.launch {
+                    withContext(Dispatchers.Default) {
+                        mainViewModel.updateWeatherForecast(et_city.text.toString())
+                    }
+                    mainScope.launch {
+                        setObserver()
+                        pb_fetch_forecast.visibility = View.GONE
+                    }
+                }
+            } else {
+                Toast.makeText(activity as MainActivity, "Please enter a city.", Toast.LENGTH_SHORT).show()
+            }
+        } else {
+            Toast.makeText(activity as MainActivity, "Please ensure network connectivity.", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
--- app/src/main/java/com/example/weatherforecast/view/MainActivity.kt
+++ app/src/main/java/com/example/weatherforecast/view/MainActivity.kt
@@ -1,98 +1,37 @@
 package com.example.weatherforecast.view
 
-import android.content.Context
 import android.os.Bundle
-import android.view.KeyEvent
 import android.view.View
-import android.view.inputmethod.EditorInfo
-import android.view.inputmethod.InputMethodManager
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
-import androidx.recyclerview.widget.DefaultItemAnimator
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.weatherforecast.R
 import com.example.weatherforecast.databinding.ActivityMainBinding
-import com.example.weatherforecast.model.WeatherResponse
-import com.example.weatherforecast.utils.isNetworkConnectionAvailable
-import com.example.weatherforecast.view.adapters.WeatherForecastRecyclerViewAdapter
+import com.example.weatherforecast.model.Forecast
 import com.example.weatherforecast.viewmodel.MainViewModel
 import com.example.weatherforecast.viewmodel.ViewModelFactory
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 
 class MainActivity : AppCompatActivity() {
 
-    private var weatherDetail: LiveData<WeatherResponse>? = null
     lateinit var activityMainBinding: ActivityMainBinding
-    private val backgroundScope = CoroutineScope(Dispatchers.IO)
-    private val mainScope = CoroutineScope(Dispatchers.Main)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        activityMainBinding = DataBindingUtil.setContentView(this@MainActivity,
-            R.layout.activity_main)
+        activityMainBinding = DataBindingUtil.setContentView(
+            this@MainActivity,
+            R.layout.activity_main
+        )
 
         activityMainBinding.viewModel =
-                    ViewModelProviders.of(this@MainActivity, ViewModelFactory()).get(MainViewModel::class.java)
+            ViewModelProviders.of(this@MainActivity, ViewModelFactory()).get(MainViewModel::class.java)
 
+        activityMainBinding.flFragmentParent.visibility = View.VISIBLE
+        supportFragmentManager.beginTransaction().add(
+            R.id.fl_fragment_parent,
+            ForecastListFragment()
+        ).commit()
 
-        rv_weather_forecast.layoutManager = LinearLayoutManager(this@MainActivity)
-        rv_weather_forecast.itemAnimator = DefaultItemAnimator()
-
-        weatherDetail = (activityMainBinding.viewModel as MainViewModel).getWeatherForecast()
-
-        if ((weatherDetail as LiveData<WeatherResponse>).value != null) {
-            if (activityMainBinding.viewModel?.getWeatherForecast()?.value is WeatherResponse.Success) {
-                window.setSoftInputMode(InputMethodManager.HIDE_NOT_ALWAYS)
-                tv_placeholder.visibility = View.GONE
-                (activityMainBinding.viewModel?.getWeatherForecast()?.value as WeatherResponse.Success).result.forecast.forecastday.let {
-                    rv_weather_forecast.adapter = WeatherForecastRecyclerViewAdapter(
-                        this@MainActivity,
-                        it
-                    )
-                }
-            }
-        }
-
-
-        et_city.setOnEditorActionListener { _, actionId, event ->
-            if ((event != null && (event.keyCode == KeyEvent.KEYCODE_ENTER)) ||
-                (actionId == EditorInfo.IME_ACTION_DONE)) {
-                fetchForecast()
-            }
-            false
-        }
-
-        tv_get_forecast.setOnClickListener { fetchForecast() }
-
-    }
-
-    private fun setObserver(){
-        (weatherDetail as LiveData<WeatherResponse>).observe(this@MainActivity,
-            Observer<WeatherResponse> { t ->
-                if (t is WeatherResponse.Success) {
-                    tv_placeholder.visibility = View.GONE
-                    rv_weather_forecast.adapter = WeatherForecastRecyclerViewAdapter(
-                        this@MainActivity,
-                        t.result.forecast.forecastday
-                    )
-                } else {
-                    tv_placeholder.visibility = View.VISIBLE
-                    Toast.makeText(this@MainActivity, (t as WeatherResponse.Error).message, Toast.LENGTH_SHORT).show()
-                    rv_weather_forecast.adapter = WeatherForecastRecyclerViewAdapter(
-                        this@MainActivity,
-                        null
-                    )
-                }
-            })
     }
 
     override fun onBackPressed() {
@@ -100,29 +39,6 @@ class MainActivity : AppCompatActivity() {
             supportFragmentManager.popBackStack()
         } else {
             super.onBackPressed()
-        }
-    }
-
-    private fun fetchForecast() {
-        if (this@MainActivity.isNetworkConnectionAvailable()) {
-            if (et_city.text.toString().trim().isNotEmpty()) {
-                val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-                imm.toggleSoftInput(0, InputMethodManager.HIDE_NOT_ALWAYS)
-                pb_fetch_forecast.visibility = View.VISIBLE
-                backgroundScope.launch {
-                    withContext(Dispatchers.Default) {
-                        (activityMainBinding.viewModel as MainViewModel).updateWeatherForecast(et_city.text.toString())
-                    }
-                    mainScope.launch {
-                        setObserver()
-                        pb_fetch_forecast.visibility = View.GONE
-                    }
-                }
-            } else {
-                Toast.makeText(this@MainActivity, "Please enter a city.", Toast.LENGTH_SHORT).show()
-            }
-        } else {
-            Toast.makeText(this@MainActivity, "Please ensure network connectivity.", Toast.LENGTH_SHORT).show()
         }
     }
 }
