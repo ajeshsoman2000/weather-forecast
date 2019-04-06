a8ceaa51f4a83f1228a54422acce2cbe2af79e2d
ajeshsoman2000 <ajeshsoman2000@gmail.com>
2019-04-06 12:16:00 +0530

    Removed old viewmodel test class andd added new empty test class. Still not getting coverage data.

---
+++ app/src/test/java/com/example/weatherforecast/viewmodel/MainViewModelTest.kt
@@ -0,0 +1,12 @@
+package com.example.weatherforecast.viewmodel
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class MainViewModelTest {
+
+    @Test
+    fun getWeatherForecast() {
+    }
+}
